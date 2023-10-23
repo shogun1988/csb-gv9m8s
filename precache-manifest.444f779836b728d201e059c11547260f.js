2426c902f99b12f0215a75363c0412f76eefa44e
Code Sandbox <hello@codesandbox.io>
2023-10-23 13:19:12 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.444f779836b728d201e059c11547260f.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "173e61621f0dc6fd9c5605d1888bdec4",
+    "url": "https://shogun1988.github.io/csb-gv9m8s/index.html"
+  },
+  {
+    "revision": "2b621e135df34e2aa394",
+    "url": "https://shogun1988.github.io/csb-gv9m8s/static/js/2.46c8e90e.chunk.js"
+  },
+  {
+    "revision": "f3ad6665260e9dcfe301",
+    "url": "https://shogun1988.github.io/csb-gv9m8s/static/js/main.b49e35f5.chunk.js"
+  },
+  {
+    "revision": "540ed2b426af4fde72c6",
+    "url": "https://shogun1988.github.io/csb-gv9m8s/static/js/runtime-main.61afa845.js"
+  }
+]);
